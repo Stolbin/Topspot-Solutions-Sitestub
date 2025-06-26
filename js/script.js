ad818f440b31d7373b12caa74426a86375d5519a
Stolbin <109080579+Stolbin@users.noreply.github.com>
2025-06-26 19:07:41 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const img = document.getElementById("main-bg");
+const isMobile = window.innerWidth <= 1020;
+
+img.src = isMobile
+  ? "./images/main_img_320_1.webp"
+  : "./images/main_img_1200_1.webp";
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("mouseenter", () => {
